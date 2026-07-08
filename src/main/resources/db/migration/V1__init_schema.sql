f494b922f86a73f94e9f11ed9754e3891c25e530
Smard-dev <sepehrmarda@gmail.com>
2026-07-09 01:50:07 +0330

    initial Spring Boot setup with Flyway migration for core schema

---
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -0,0 +1,65 @@
+
+CREATE TABLE users(
+                      user_id INT AUTO_INCREMENT,
+                      first_name VARCHAR(50) NOT NULL,
+                      last_name VARCHAR(50) NOT NULL ,
+                      national_id CHAR(10) NOT NULL UNIQUE ,
+                      phone_number VARCHAR(11) NOT NULL ,
+                      email VARCHAR(100),
+                      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                      status BOOLEAN DEFAULT TRUE,
+                      date_of_birth DATE  ,
+                      CONSTRAINT pkUsers PRIMARY KEY (user_id)
+);
+
+
+CREATE TABLE currency(
+                         currency_id INT AUTO_INCREMENT,
+                         currency_code CHAR(3) NOT NULL UNIQUE ,
+                         currency_name  VARCHAR(20) NOT NULL ,
+                         is_currency_active BOOLEAN DEFAULT TRUE NOT NULL ,
+                         CONSTRAINT pk_currencies PRIMARY KEY (currency_id)
+);
+CREATE TABLE account_type(
+                             account_type_id INT AUTO_INCREMENT,
+                             account_type_name VARCHAR(20),
+                             is_type_active BOOLEAN DEFAULT TRUE NOT NULL ,
+                             CONSTRAINT pk_type PRIMARY KEY (account_type_id)
+);
+create table accounts(
+                         account_id int auto_increment,
+                         user_id int not null ,
+                         account_number char(16) not null unique ,
+                         currency_id INT default 1,
+                         balance decimal(15,2) default 0.00,
+                         account_type_id int default 1 ,
+                         status boolean default true  ,
+                         created_at timestamp default current_timestamp,
+
+                         constraint pk_accounts primary key (account_id),
+                         constraint fk_accounts_users foreign key (user_id) references users (user_id),
+                         constraint fk_accounts_currency foreign key (currency_id) references currency (currency_id),
+                         constraint fk_account_type foreign key (account_type_id) references account_type (account_type_id),
+                         constraint chk_accont_balance CHECK ( balance>= 0  )
+);
+
+
+CREATE TABLE transaction_type(
+                                 transaction_type_id INT AUTO_INCREMENT ,
+                                 type_name VARCHAR(10) NOT NULL ,
+                                 is_type_active BOOLEAN DEFAULT TRUE NOT NULL ,
+                                 CONSTRAINT pk_type PRIMARY KEY (transaction_type_id)
+);
+CREATE TABLE transactions(
+                             transaction_id INT AUTO_INCREMENT,
+                             account_id INT NOT NULL ,
+                             amount DECIMAL(15,2) NOT NULL ,
+                             transaction_type_id INT NOT NULL ,
+                             description VARCHAR(255),
+                             transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+                             CONSTRAINT pk_transactions PRIMARY KEY (transaction_id),
+                             CONSTRAINT fk_transaction_account FOREIGN KEY (account_id) REFERENCES accounts (account_id),
+                             CONSTRAINT fk_transaction_type FOREIGN KEY (transaction_type_id) REFERENCES transaction_type (transaction_type_id),
+                             CONSTRAINT chk_amount CHECK ( amount > 0)
+);
